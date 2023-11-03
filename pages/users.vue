1125240e3c1506ad9845604482dac5509e9bda98
tynmarket <tynmarket@gmail.com>
2023-11-03 16:13:36 +0900

    エラー処理

--- pages/users.vue
+++ pages/users.vue
@@ -1,8 +1,23 @@
 <template>
   <h1>Usersページ</h1>
   <p>Count: {{ counter }}</p>
-  <NuxtPage />
+  <NuxtErrorBoundary @error="someErrorLogger">
+    <NuxtPage />
+    <template #error="{ error }">
+      <p>{{ error }}</p>
+      <button @click="resetError(error)">Clear Error</button>
+    </template>
+</NuxtErrorBoundary>
 </template>
 <script setup>
 const counter = useCounter2();
+
+const resetError = (error) => {
+  error.value = null;
+};
+
+const someErrorLogger = (error) => {
+  //取得したエラーで別の処理を行う
+  console.log('someErrorLogger', error);
+};
 </script>
--- pages/users/list.vue
+++ pages/users/list.vue
@@ -1,4 +1,13 @@
 <template>
   <h1>User Listページ</h1>
   <NuxtLink to="1">ID 1</NuxtLink><br>
+  <p>User List Count: {{ counter }}</p>
+  <div><button @click="inc">+</button></div>
 </template>
+<script setup>
+const counter = ref(0);
+const inc = () => {
+  throw createError('エラー発生');
+  counter.value++;
+};
+</script>
