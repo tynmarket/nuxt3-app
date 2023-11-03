58148f4f6adea81b66a7e8a13e1a11abf5c743b6
tynmarket <tynmarket@gmail.com>
2023-11-03 15:42:19 +0900

    Data Fetch

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,5 +3,6 @@
     <NuxtLink to="/">Home</NuxtLink><br>
     <NuxtLink to="/about">About</NuxtLink><br>
     <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink><br>
+    <NuxtLink to="/posts">Posts</NuxtLink><br>
   </nav>
 </template>
---
+++ pages/posts/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+  /*
+  const { data: posts, error, refresh, } = await useFetch(
+    'https://jsonplaceholder.typicode.com/posts/'
+  );
+  */
+  console.log('useNuxtApp().payload.data');
+  console.log(useNuxtApp().payload.data);
+  const { data: posts, error } = await useAsyncData('posts',() =>
+    $fetch('https://jsonplaceholder.typicode.com/posts/')
+  );
+  console.log(('error', error.value));
+</script>
+<template>
+  <div>
+    <h1>Posts一覧</h1>
+    <button @click="refresh()">再取得</button>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
